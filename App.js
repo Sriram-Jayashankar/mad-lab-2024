9cec2d086608d7c600944bbe919e9d97ea930c13
Sriram Jayashankar <sriramjayashankar@gmail.com>
2023-12-18 00:15:14 +0530

    Refactor App.js to include CurrentWeather component

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+import CurrentWeather from "./src/components/CurrentWeather"
+import {React} from "react"
+import {View,StyleSheet} from "react-native"
+const app=()=>
+{
+  return(
+    <View style={styles.  mainview}>
+    <CurrentWeather/>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles=StyleSheet.create(
+  {
+    mainview:{
+      flex:1
+    }
+  }
+)
+export default app
---
+++ src/components/UpdateWeather.js
@@ -0,0 +1,12 @@
+import {React} from "react"
+import {View,SafeAreaView} from "react-native"
+
+import React from 'react'
+data=[]
+export default function UpdateWeather() {
+  return (
+    <SafeAreaView>
+
+    </SafeAreaView>
+  )
+}
