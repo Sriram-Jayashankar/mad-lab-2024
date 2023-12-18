a49a4d147c2749d2cc58bb9cf6db0bfa2c0c4af8
Sriram Jayashankar <sriramjayashankar@gmail.com>
2023-12-18 17:33:06 +0530

    Add FlatList and Item component to UpdateWeather.js

--- src/components/UpdateWeather.js
+++ src/components/UpdateWeather.js
@@ -1,12 +1,46 @@
-import {React} from "react"
-import {View,SafeAreaView} from "react-native"
+import { React } from "react"
+import { View, SafeAreaView, StyleSheet, FlatList, StatusBar } from "react-native"
 
 import React from 'react'
-data=[]
+const data = [{
+  id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+  title: 'First Item',
+},
+{
+  id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+  title: 'Second Item',
+},
+{
+  id: '58694a0f-3da1-471f-bd96-145571e29d72',
+  title: 'Third Item',
+}]
+
+const Item = (props) => {
+  const { id, title } = props.data
+  return (<><Text>{id}</Text>
+    <Text>{title}</Text></>)
+}
 export default function UpdateWeather() {
   return (
     <SafeAreaView>
+      <View styles={styles.container}>
+        <FlatList data={data} renderItem={(item) => { <Item data={item} /> }} keyExtractor={(item) => { item.id }} />
+      </View>
 
     </SafeAreaView>
   )
 }
+const styles = StyleSheet.create(
+  {
+    item: {
+      backgroundColor: "pink",
+      textAlign: "center",
+      color: "black"
+    },
+    container: {
+      alignItems: "center",
+      marginTop: StatusBar.currentHeight || 0, //this is to ensure that the content is in safe region thats hwy the margin top is the height of the status bar
+      flex: 1
+    }
+  }
+)
